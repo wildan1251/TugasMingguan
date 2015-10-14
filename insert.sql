f423badfd870ece7c400b998e220a17f092a4d7e
rahmad-karyadi <ujangadi1@mail.com>
2015-10-14 22:35:33 +0700

    Pak Saya Nambah data di file insert.sql

--- insert.sql
+++ insert.sql
@@ -1,2 +1,3 @@
 insert into tugas_mingguan values ("10104007","Adam MB","2015-10-28 09:31"),
 /* baris 1 hanya contoh perintah yang dimasukan, silakan tambahkan data anda setelah baris ini */
+insert into tugas_mingguan values ("10113117","Rian Saepuloh","2015-10-14 22:33"),
