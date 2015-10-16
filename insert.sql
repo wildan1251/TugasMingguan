057c23153940fe0918c8b12239e2f1a2a5409e38
farhanarismunandar <farhanarismunandar@gmail.com>
2015-10-16 21:59:23 +0700

    edit farhan

--- insert.sql
+++ insert.sql
@@ -1,2 +1,2 @@
-insert into tugas_mingguan values ("10104007","Adam MB","2015-10-28 09:31"),
+insert into tugas_mingguan values ("10113052","Farhan Aris Munandar","2015-10-16 21.29"),
 /* baris 1 hanya contoh perintah yang dimasukan, silakan tambahkan data anda setelah baris ini */
