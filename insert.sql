fed7ea7ed058c1edf669db052953bbd744696858
riansaepuloh <riansaepuloh92@gmail.com>
2015-10-17 08:15:27 +0700

    Rian Uabah tanggal sama Jamnya

--- insert.sql
+++ insert.sql
@@ -1,4 +1,4 @@
 insert into tugas_mingguan values ("10113052","Farhan Aris Munandar","2015-10-16 21.29"),
 insert into tugas_mingguan values ("10113075","Mochamad Arry Akbar","2015-10-17 07.22"),
 /* baris 1 hanya contoh perintah yang dimasukan, silakan tambahkan data anda setelah baris ini */
-insert into tugas_mingguan values ("10113117","Rian Saepuloh","2015-10-14 22:33"),
+insert into tugas_mingguan values ("10113117","Rian Saepuloh","2015-10-77 07:45"),
