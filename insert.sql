38a355079a5fcea8c4f30a5b3a94194e3e42797c
unknown <herawatinurhanifahh@gmail.com>
2015-10-17 09:10:41 +0700

    edit

--- insert.sql
+++ insert.sql
@@ -2,3 +2,4 @@ insert into tugas_mingguan values ("10113052","Farhan Aris Munandar","2015-10-16
 insert into tugas_mingguan values ("10113075","Mochamad Arry Akbar","2015-10-17 07.22"),
 /* baris 1 hanya contoh perintah yang dimasukan, silakan tambahkan data anda setelah baris ini */
 insert into tugas_mingguan values ("10113117","Rian Saepuloh","2015-10-77 07:45"),
+insert into tugas_mingguan values ("10113069","Herawati Nur Hanifah","2015-10-17 08:59"),
