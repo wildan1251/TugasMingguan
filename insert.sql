37e6da8000f2fe0c305a2f34bfdcb9fdfb77a344
Pedri24 <pedrihidayat29@gmail.com>
2015-10-17 11:51:45 +0700

    edit

---
+++ insert.sql
@@ -0,0 +1,7 @@
+insert into tugas_mingguan values ("10113052","Farhan Aris Munandar","2015-10-16 21.29"),
+insert into tugas_mingguan values ("10113075","Mochamad Arry Akbar","2015-10-17 07.22"),
+/* baris 1 hanya contoh perintah yang dimasukan, silakan tambahkan data anda setelah baris ini */
+insert into tugas_mingguan values ("10113117","Rian Saepuloh","2015-10-77 07:45"),
+insert into tugas_mingguan values ("10113069","Herawati Nur Hanifah","2015-10-17 08:59"),
+insert into tugas_mingguan values ("10113448","Romi Wadi Saputra","2015-10-17 11:22"),
+insert into tugas_mingguan values ("10113479","Pedri Nuraprili Hidayat","2015-10-17 11:43"),
