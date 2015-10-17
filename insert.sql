a5a8598852f119883f07912e2ee55cc8a0a6ecf2
ROMI WADI SAPUTRA <romychelsea27@gmail.com>
2015-10-17 11:22:44 +0700

    push romi

--- insert.sql
+++ insert.sql
@@ -3,3 +3,4 @@ insert into tugas_mingguan values ("10113075","Mochamad Arry Akbar","2015-10-17 
 /* baris 1 hanya contoh perintah yang dimasukan, silakan tambahkan data anda setelah baris ini */
 insert into tugas_mingguan values ("10113117","Rian Saepuloh","2015-10-77 07:45"),
 insert into tugas_mingguan values ("10113069","Herawati Nur Hanifah","2015-10-17 08:59"),
+insert into tugas_mingguan values ("10113448","Romi Wadi Saputra","2015-10-17 11:22"),
