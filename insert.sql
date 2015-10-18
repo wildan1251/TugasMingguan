2a5c5399344243f02c0cb2347d8c8f0f99633995
BayuWijayaPermanaPutra <bayu_wpp@programmer.net>
2015-10-18 22:41:57 +0700

    Penambahan Data 10113218 BayuWPP

--- insert.sql
+++ insert.sql
@@ -1,6 +1,7 @@
 ﻿insert into tugas_mingguan values ("10113052","Farhan Aris Munandar","2015-10-16 21.29"),
 insert into tugas_mingguan values ("10113075","Mochamad Arry Akbar","2015-10-17 07.22"),
 /* baris 1 hanya contoh perintah yang dimasukan, silakan tambahkan data anda setelah baris ini */
+"10113218","Bayu Wijaya Permana Putra"
 <<<<<<< HEAD
 insert into tugas_mingguan values ("10113049”,”Friendy”,”2015-10-28 09:31"),
 =======
